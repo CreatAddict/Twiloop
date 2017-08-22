c02bedcb51d3615c108479afaf211fdd05e00350
CreaDepa <michel@creadepa.fr>
2017-08-22 20:19:10 +0200

    Correction fonction Twiloop_nombre_pair

--- libs/Twiloop_nombre_pair.php
+++ libs/Twiloop_nombre_pair.php
@@ -6,6 +6,5 @@
  * @return <type> Returne true si paire si non false
  */
 function Twiloop_nombre_pair($number) {
-    if($number%2 == 0) return true;
-    else return false;
+    return $number%2;
 }

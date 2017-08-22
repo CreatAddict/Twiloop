8c561fbfd9292e87029c939853ca4f890a6b8aae
CreaDepa <michel@creadepa.fr>
2017-08-22 20:28:25 +0200

    Ajout function

---
+++ libs/Twiloop_sql_select.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+* Select DB
+* @param type $DB
+* @param type $champ
+* @param type $condition
+* @param type $fetchall
+* @param type $disting
+* @return object
+*/
+function Twiloop_sql_select($DB, $champ, $condition = '', $data = '', $fetchall = false, $disting = false) {
+    $req = '';
+
+    if(!empty($condition)) {
+        $condition = " WHERE {$condition}";
+    }
+
+    if($disting === true) {
+        $champ = "DISTING {$champ}";
+    }
+
+    try {
+        $req = $DB->prepare("SELECT {$champ} FROM {$table} {$condition}");
+        $req->execute($reqValues);
+        if($fetchall === true) {
+            return $req->fetchAll($data);
+        } else {
+            return $req->fetch($data);
+        }
+    } catch (PDOException $e) {
+        echo $e;
+        return FALSE;
+    }
+}

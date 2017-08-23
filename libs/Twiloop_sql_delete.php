4eca953391f4fa0700357a484d94404ec5240062
CreaDepa <michel@creadepa.fr>
2017-08-23 20:56:49 +0200

    Ajout fonction

---
+++ libs/Twiloop_sql_delete.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Delete DB
+ * @param type $DB
+ * @param type $table
+ * @param type $condition
+ * @return boolean
+ */
+function Twiloop_sql_update($DB, $table, $condition) {
+    $req = '';
+
+    if(!empty($condition)) {
+        $condition = " WHERE {$condition}";
+    }
+
+    try {
+        $req = $DB->prepare("DELETE FROM {$table}{$condition}");
+        $req->execute();
+        return $req->rowCount();
+    } catch (PDOException $e) {
+        echo $e;
+        return FALSE;
+    }
+}
+
+?>

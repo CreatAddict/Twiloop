cd78df8b931d6d155941ce6749eec0bc455b6621
CreaDepa <michel@creadepa.fr>
2017-08-23 11:47:05 +0200

    Correction

--- libs/Twiloop_sql_select.php
+++ libs/Twiloop_sql_select.php
@@ -1,35 +1,34 @@
-<?php
+<?php 
 
 /**
 * Select DB
 * @param type $DB
 * @param type $champ
-* @param type $condition
+* @param type $extend_sql
 * @param type $fetchall
 * @param type $disting
 * @return object
 */
-function Twiloop_sql_select($DB, $table, $champ, $condition = '', $data = '', $fetchall = false, $disting = false) {
-    $req = '';
 
-    if(!empty($condition)) {
-        $condition = " WHERE {$condition}";
-    }
+function Twiloop_sql_select($DB, $table, $champ, $extend_sql = '', $data = [], $fetchall = false, $disting = false) {
+    $req = '';
 
     if($disting === true) {
         $champ = "DISTING {$champ}";
     }
 
     try {
-        $req = $DB->prepare("SELECT {$champ} FROM {$table} {$condition}");
-        $req->execute($reqValues);
+        $req = $DB->prepare("SELECT {$champ} FROM {$table} {$extend_sql}");
+        $req->execute($data);
         if($fetchall === true) {
-            return $req->fetchAll($data);
+            return $req->fetchAll();
         } else {
-            return $req->fetch($data);
+            return $req->fetch();
         }
     } catch (PDOException $e) {
         echo $e;
         return FALSE;
     }
 }
+
+?>

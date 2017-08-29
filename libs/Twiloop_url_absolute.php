d99e9d0976b48462d235c9a224ab484814ce0b7f
CreaDepa <michel@creadepa.fr>
2017-08-29 15:13:38 +0200

    Refactoring

--- libs/Twiloop_url_absolute.php
+++ libs/Twiloop_url_absolute.php
@@ -1,13 +1,21 @@
 <?php
 
-function Twiloop_url_absolute() {
+/**
+ * URL Abolue
+ * @return $url_absolu
+ */
+
+
+function Twiloop_url_absolute()
+{
     $url_absolu = explode('/', $_SERVER["REQUEST_URI"]);
 
     $url_count = count($url_absolu) - 1;
     $data_url = array();
     for ($i = 0; $i != $url_count; $i++) {
-        if ($url_absolu[$i] != $langue)
+        if ($url_absolu[$i] != $langue) {
             $data_url[] = $url_absolu[$i];
+        }
     }
     $url_absolu = implode('/', $data_url);
 

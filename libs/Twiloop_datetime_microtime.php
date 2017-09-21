b8e3c3e41a66dd8b49f4e0aec2896fdd5ed38a68
CreaDepa <michel@creadepa.fr>
2017-09-21 22:43:34 +0200

    Ajout function datetime_microtime

--- libs/Twiloop_datetime_microtime.php
+++ libs/Twiloop_datetime_microtime.php
@@ -9,7 +9,7 @@
 function Twiloop_datetime_microtime($format)
 {
     $time = microtime(true);
-    $micro_time=sprintf("%06d",($time - floor($time)) * 1000000);
+    $micro_time = sprintf("%06d",($time - floor($time)) * 1000000);
     $date = new DateTime( date('Y-m-d H:i:s.'.$micro_time,$time) );
     return $date->format($format);
 }
